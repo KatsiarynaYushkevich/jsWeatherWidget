104c6a679f63a0cb8d5374a0d8213b384226f7fe
KatsiarynaYushkevich <e.yushkevich30@gmail.com>
2025-02-18 13:26:41 +0300

    add task

--- js/app.js
+++ js/app.js
@@ -1,34 +1,58 @@
-/*По данному url расположена задача.
-В html предусмотреть <div></div>
-Достать с сервера заголовок задачи и отобразить его в div.
-*/
-const div = document.querySelector("div");
+const topBlock = document.querySelector(".top_block");
+const bottomBlock = document.querySelector(".bottom_block");
 
-let connection = new XMLHttpRequest();
-connection.open('GET', 'https://jsonplaceholder.typicode.com/todos/1');
-connection.send();
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=Minsk&units=metric&appid=a94d0a5ac08570add4b47b8da933f247')
+.then((response) =>  response.json())
+.then((data) =>   {const currentWeather = data.list[0];
+
+const iconCode = currentWeather.weather[0].icon;
+const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+console.log(currentWeather);
+
+addWeather();
+
+function addWeather(){
+    let date = new Date(currentWeather.dt * 1000).toLocaleDateString('ru-RU');
+    topBlock.innerHTML = `    
+    <div class="city_time"> 
+    <span>${data.city.name}</span>
+    <span>${date}</span>
+    </div>
+    <div class="weather">
+    <img src="${iconUrl}">
+    <span>${currentWeather.weather[0].main}</span>
+    <span>${currentWeather.main.temp} °C</span>
+    </div>
+    <div class="wind_speed"> 
+    <span>Speed</span>
+    <span>${currentWeather.wind.speed} m/s</span>
+    </div>
+    `  
+}
 
 
-connection.onload = function(){
-   const responseObj = JSON.parse(connection.response);
-    div.innerHTML = responseObj.title;
-};
+function showWeatherInfo(forecast){
 
-/*Запросом на сервер по url https://jsonplaceholder.typicode.com/todos достать задачи.
-Отобразить первые 20 задач списком ul на странице. Содержимое каждого li - поле title объекта задачи.
-*/
-const ulElement = document.querySelector("ul");
+    const forecastDate = new Date(forecast.dt * 1000).toLocaleDateString('ru-RU');
+    const forecastIconCode = forecast.weather[0].icon;
+    const forecastIconUrl = `https://openweathermap.org/img/wn/${forecastIconCode}@2x.png`;
 
-let anotherConnection = new XMLHttpRequest();
-anotherConnection.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-anotherConnection.send();
+    const weatherDiv = document.createElement("div");
+    weatherDiv.classList.add("past_weather");
+    weatherDiv.innerHTML = `
+    <div>
+    <span>${forecastDate}</span>
+    <span></span>
+    </div>
+    <img src="${forecastIconUrl}">
+    <span>${forecast.main.temp} °C</span>
+    `;
+    bottomBlock.appendChild(weatherDiv);
+}
 
-anotherConnection.onload = function(){
-    const responseArray = JSON.parse(anotherConnection.response);
-    const liElement = document.createElement("li");
-    for(let i = 0; i < 20; i++){
-        const liElement = document.createElement("li");
-        liElement.innerHTML = responseArray[i].title;
-        ulElement.appendChild(liElement);
-    }
-};
+
+for (let i = 0; i < data.list.length; i += 8) {
+    const forecast = data.list[i];
+    showWeatherInfo(forecast);
+}});
+
